59540d74e2e68473b239574ecb1eb71c9c0c281c
Eldintaro Farrandi <eldintarofarrandi@gmail.com>
2024-12-22 22:40:32 +0700

    add

--- Tugas2/case4/collector/scripts/weather_collector.sh
+++
@@ -1,9 +0,0 @@
-docker rm -f weather_collector
-
-docker container run -d \
-  --name weather_collector \
-  --network mynetwork \
-  -e DELAY=3600 \
-  -v "$(pwd)/scripts:/script" \
-  alpine:3.18 \
-  /bin/sh -c "apk update && apk add --no-cache mysql-client curl jq && /bin/sh /script/getweather.sh"
